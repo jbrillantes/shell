91a1b553c8244951ae2086fde7c25199843f7820
jbrillantes <jacqueline.brillantes@mail.utoronto.ca>
2026-03-03 19:41:46 -0500

    Generate inventory.txt from processed data files
    Create inventory.txt file listing all files in processed subfolders

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -64,6 +64,7 @@ rf -rf ./data
 >>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+mkdir -p data/inventory.txt
 find ./data/processed -mindepth 2 -type f > ./data/inventory.txt
 
 ###########################################
