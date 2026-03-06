50f9674e7d696b24b43284d4ab98bb08b1cc02e4
jbrillantes <jacqueline.brillantes@mail.utoronto.ca>
2026-03-06 13:19:16 -0500

    Fix path in inventory file creation command
    fixed question #8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -61,7 +61,7 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 mkdir -p data/inventory.txt
-find ./data/processed -mindepth 2 -type f > ./data/inventory.txt
+find data/processed -mindepth 2 -type f > ./data/inventory.txt
 
 ###########################################
 
